5db1e367adf241ed30a54da62e263255f198d73e
Victor Martin <vtrmrtn@gmail.com>
2019-08-07 12:46:36 +0200

    fix pwa

--- js/main.js
+++ js/main.js
@@ -7,9 +7,23 @@ window.onload = () => {
     let deferredPrompt;
 
     window.addEventListener('beforeinstallprompt', (e) => {
-        alert("Pum pum 2 truenos")
         deferredPrompt = e;
+    });
 
+
+    document.getElementsByClassName('js-install-button')[0].addEventListener('click', (e) => {
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+            .then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
     });
 
 
