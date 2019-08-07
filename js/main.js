24e8f2e14e8d161ccf44def330b96d5975f76d15
Victor Martin <vtrmrtn@gmail.com>
2019-08-07 10:48:27 +0200

    Pero si esto es tu repo sosiso

--- js/main.js
+++ js/main.js
@@ -4,22 +4,41 @@ window.onload = () => {
         navigator.serviceWorker.register('./sw.js');
     }
 
-    fetch()
-
-
-
     const myHeaders = new Headers();
 
-    const myInit = { method: 'GET',
+    const myInit = {
+        method: 'GET',
         headers: myHeaders,
         mode: 'cors',
-        cache: 'default' };
+        cache: 'default'
+    };
 
-    const myRequest = new Request('https://www.pccomponentes.com/ajax_nc/get?idArticle=53216', myInit);
+    const myRequest = new Request('/articles.json', myInit);
 
-    fetch(myRequest).then(function(response) {
-        console.log(response);
-    });
+    fetch(myRequest)
+        .then(function (response){ return  response.json()})
+        .then(function (data) {
+            console.log(data);
+            const articlesContainer =  document.getElementsByClassName('articles-container')[0];
+            data.map(function (article) {
+                let container = document.createElement('span');
+                container.className = "article"
+                let image = document.createElement("img");
+                image.src = "image/article.gif"
+
+                let name = document.createElement("div");
+                name.innerText = article.buyBox.nameProductBuyBox;
+
+                let price = document.createElement('div');
+                price.innerText = article.price.price + '€';
+
+                container.append(image);
+                container.append(name);
+                container.append(price);
+
+                articlesContainer.append(container);
+            })
+        });
 
 
 }
