424b49493ac713c6205764f22f29961b1bdc5251
Victor Martin <vtrmrtn@gmail.com>
2019-08-07 09:51:10 +0200

    Probando

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 window.onload = () => {
     'use strict';
-
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('./sw.js');
+        navigator.serviceWorker.register('./sw.js');
     }
+
+    fetch()
+
+
+
+    const myHeaders = new Headers();
+
+    const myInit = { method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default' };
+
+    const myRequest = new Request('www.pccomponentes.com/ajax_nc/get?idArticle=53216', myInit);
+
+    fetch(myRequest).then(function(response) {
+        console.log(response);
+    });
+
+
 }
+
