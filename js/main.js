190e9414300d9491c1d5e2081e0c98883dfb9198
Victor Martin <vtrmrtn@gmail.com>
2019-08-07 13:31:36 +0200

    fix pwa

--- js/main.js
+++ js/main.js
@@ -1,13 +1,47 @@
 window.onload = () => {
     'use strict';
+
+
+
+
+    /* Only register a service worker if it's supported */
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./sw.js');
+
     }
 
-    let deferredPrompt;
 
-    window.addEventListener('beforeinstallprompt', (e) => {
-        e.prompt()
+    const divInstall = document.getElementById('installContainer');
+    const butInstall = document.getElementById('butInstall');
+
+
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+        console.log('👍', 'beforeinstallprompt', event);
+        // Stash the event so it can be triggered later.
+        window.deferredPrompt = event;
+        // Remove the 'hidden' class from the install button container
+        divInstall.classList.toggle('hidden', false);
+    });
+
+    butInstall.addEventListener('click', () => {
+        console.log('👍', 'butInstall-clicked');
+        const promptEvent = window.deferredPrompt
+        if (!promptEvent) {
+            // The deferred prompt isn't available.
+            return;
+        }
+        // Show the install prompt.
+        promptEvent.prompt();
+        // Log the result
+        promptEvent.userChoice.then((result) => {
+            console.log('👍', 'userChoice', result);
+            // Reset the deferred prompt variable, since
+            // prompt() can only be called once.
+            window.deferredPrompt = null;
+            // Hide the install button.
+            divInstall.classList.toggle('hidden', true);
+        });
     });
 
 
